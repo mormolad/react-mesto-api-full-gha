5c2c00065a4616057882567e86404586a38128c2
Perevalov Anton <mormolad@yandex.ru>
2023-09-28 22:33:26 +0700

    env done

--- backend/app.js
+++ backend/app.js
@@ -33,6 +33,11 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(helmet());
 app.disable("x-powered-by");
 app.use(requestLogger); // подключаем логгер запросов
+app.get("/crash-test", () => {
+  setTimeout(() => {
+    throw new Error("Сервер сейчас упадёт");
+  }, 0);
+});
 app.use(routerAuth);
 app.use(auth);
 app.use(routerUser);
